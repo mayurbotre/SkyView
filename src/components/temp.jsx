710d2a7fa4be8175cd6a7a072e7933e8f188725d
mayurbotre <mayurbotre95@gmail.com>
2024-04-26 23:46:13 +0530

    Added api key

--- src/components/temp.jsx
+++ src/components/temp.jsx
@@ -8,7 +8,7 @@ const Temp = () => {
 
   const getWeatherInfo = async () => {
     try {
-      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid={WriteYourAPIKey}`;
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid={d6827ef4b223f4ee11c84fae2d7fa5c3}`;
 
       let res = await fetch(url);
       let data = await res.json();
